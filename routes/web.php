09f37e363d323a5095ea2dbf8f9f1adab94588ee
Joao Nakano <joaovynak@gmail.com>
2025-05-12 21:30:03 -0300

    initial route system convention

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rotas do projeto
+
+// GET
+Route::get("/");
+Route::get("/login");
+Route::get("/register");
+Route::get("/games");
+Route::get("/games/register");
+Route::get("/games/{id}");
+Route::get("/user");
+Route::get("/users/{id}");
+Route::get("/dashboard");
+
+// PUT
+Route::put("/games/{uuid}");
+Route::put("/users/{uuid}");
+
+// DELETE
+Route::delete("/games/{uuid}");
+Route::delete("/users/{uuid}");
+
+// CREATE
+Route::post("/register");
+Route::post("/games/register");
